81dfe10f92d7b2383d08ba00872410718e9ce614
rocalabern <roc.alabern@gmail.com>
2015-01-23 11:31:55 +0100

    .

--- R/h2oplot.R
+++ R/h2oplot.R
@@ -1,4 +1,4 @@
-#' h2oWebStoreView
+#' h2oPlotResults
 #' @export
 h2oPlotResults <- function(
   model_score,
@@ -71,4 +71,105 @@ h2oPlotResults <- function(
   print(model)
   cat(paste0("best_cutoff: ",results@model$best_cutoff,"\n"))
   print(results@model$confusion)
+}
+
+#' h2oPlotGain
+#' @export
+h2oPlotGain <- function(
+  model_score,
+  real_target,
+  results,
+  resultsAUC,
+  model,
+  strExportFile = NULL,
+  width=800,
+  height=800) {
+
+  if (!is.null(strExportFile) && strExportFile!="") png(paste0(strExportFile, '_GAIN.png'),width=width,height=height)
+  p <- ggplot() +
+    geom_line(aes(x=c(0,1),y=c(0,1)), color=rgb(0,0,0,0.2)) +
+    geom_line(aes(x=c(0,results@gains$Quantile,1),y=c(0,results@gains$Cume.Pct.Total.Lift,1)),
+              color=rgb(0.1,0.1,0.6,0.4)) +
+    geom_point(aes(x=c(0,results@gains$Quantile,1),y=c(0,results@gains$Cume.Pct.Total.Lift,1)),
+               size=2, color=rgb(0.1,0.1,0.6,0.6)) +
+    ggtitle("Cum. Gain") +
+    ylab("% responses") +
+    xlab("% customers")
+  plot(p)
+  if (!is.null(strExportFile) && strExportFile!="") dev.off()
+}
+
+
+#' h2oPlotROC
+#' @export
+h2oPlotROC <- function(
+  model_score,
+  real_target,
+  results,
+  resultsAUC,
+  model,
+  strExportFile = NULL,
+  width=800,
+  height=800) {
+
+  indOpt = which(resultsAUC$aucdata$thresholds==results@model$best_cutoff)
+
+  if (!is.null(strExportFile) && strExportFile!="") png(paste0(strExportFile, '_ROC.png'),width=width,height=height)
+  df1 = data.frame(x=1-resultsAUC$aucdata$specificity[-indOpt], y=resultsAUC$aucdata$recall[-indOpt])
+  df2 = data.frame(x=1-resultsAUC$aucdata$specificity[indOpt], y=resultsAUC$aucdata$recall[indOpt])
+  p <- ggplot() +
+    geom_line(aes(x=c(0,1),y=c(0,1)), color=rgb(0,0,0,0.2)) +
+    geom_line(aes(x=1-resultsAUC$aucdata$specificity,y=resultsAUC$aucdata$recall),
+              color=rgb(0.1,0.1,0.6,0.4)) +
+    geom_point(data=df1, aes(x=x,y=y),
+               size=2, color=rgb(0.1,0.1,0.6,0.6)) +
+    geom_point(data=df2, aes(x=x,y=y),
+               size=5, color=rgb(0.1,0.6,0.4,0.6)) +
+    ggtitle("ROC curve") +
+    ylab("recall") +
+    xlab("1 - specificity")
+  plot(p)
+  if (!is.null(strExportFile) && strExportFile!="") dev.off()
+
+  if (!is.null(strExportFile) && strExportFile!="") png(paste0(strExportFile, '_RelativeImportance.png'),width=width,height=height)
+  df = data.frame(x = rownames(model@model$varimp), y = model@model$varimp$"Relative importance")
+  df = df[with(df, order(y)), ]
+  df = df[df$y!=0, ]
+  df$x <- factor(df$x, levels = df$x, ordered = TRUE)
+  p <- ggplot() +
+    geom_bar(data=df, aes(x=x, y=y), binwidth = 0.05, fill=rgb(0.85,0.72,0.2,0.5), color="black", stat="identity") +
+    coord_flip() +
+    ggtitle("Model") +
+    ylab("Relative importance") +
+    xlab("Variables")
+  plot(p)
+  if (!is.null(strExportFile) && strExportFile!="") dev.off()
+}
+
+
+#' h2oPlotRelativeImportance
+#' @export
+h2oPlotRelativeImportance <- function(
+  model_score,
+  real_target,
+  results,
+  resultsAUC,
+  model,
+  strExportFile = NULL,
+  width=800,
+  height=800) {
+
+  if (!is.null(strExportFile) && strExportFile!="") png(paste0(strExportFile, '_RelativeImportance.png'),width=width,height=height)
+  df = data.frame(x = rownames(model@model$varimp), y = model@model$varimp$"Relative importance")
+  df = df[with(df, order(y)), ]
+  df = df[df$y!=0, ]
+  df$x <- factor(df$x, levels = df$x, ordered = TRUE)
+  p <- ggplot() +
+    geom_bar(data=df, aes(x=x, y=y), binwidth = 0.05, fill=rgb(0.85,0.72,0.2,0.5), color="black", stat="identity") +
+    coord_flip() +
+    ggtitle("Model") +
+    ylab("Relative importance") +
+    xlab("Variables")
+  plot(p)
+  if (!is.null(strExportFile) && strExportFile!="") dev.off()
 }
--- R/h2otools.R
+++ R/h2otools.R
@@ -191,44 +191,6 @@ h2oToRarray <- function (h2oParsedData, numeric=TRUE, ...) {
   return (array)
 }
 
-#' h2oPerformanceAUC
-#' @title h2oPerformanceAUC
-#' @description
-#' AUC model performance evaluation
-#' @details
-#' This method returns AUC model performance evaluation.
-#' @export
-h2oPerformanceAUC <- function (h2oServer, dataReal, keyReal, dataModel, keyModel, criterion = "maximum_F1") {
-  url <- paste0("http://", h2oServer@ip, ":", h2oServer@port,
-                "/2/AUC?actual=",dataReal,"&vactual=",keyReal,
-                "&predict=",dataModel,"&vpredict=",keyModel,
-                "&thresholds=&threshold_criterion=",criterion)
-  print(url)
-  response <- getURL(url)
-  invisible(fromJSON(response))
-}
-
-#' h2oWebAUC
-#' @title h2oWebAUC
-#' @description
-#' Web AUC model evaluation
-#' @details
-#' Opens H2O url for AUC model evaluation.
-#' @export
-h2oWebAUC <- function (dataReal, keyReal, dataModel, keyModel, criterion = "maximum_F1", ip=NULL, port=NULL, h2oServer=NULL) {
-  if (is.null(h2oServer) && is.null(ip) && is.null(port)) stop("You must give some parameters: h2oServer or ip and port")
-  if(!is.null(h2oServer)) {
-    ip = h2oServer@ip
-    port = h2oServer@port
-  }
-  url <- paste0("http://", ip, ":", port,
-                "/2/AUC.html?actual=",dataReal,"&vactual=",keyReal,
-                "&predict=",dataModel,"&vpredict=",keyModel,
-                "&thresholds=&threshold_criterion=",criterion)
-  print(url)
-  browseURL(url)
-}
-
 #' h2oWebStoreView
 #' @title h2oWebStoreView
 #' @description
